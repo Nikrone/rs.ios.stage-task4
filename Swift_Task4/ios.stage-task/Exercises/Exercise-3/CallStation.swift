07da855871d641306b37c03594cc7e5d99f40b63
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-06-07 21:45:23 +0300

    feat: Exercise 3, not fully implemented

--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -1,18 +1,25 @@
 import Foundation
 
-final class CallStation { }
+final class CallStation {
+    var userArray = [User]()
+    var callsArray = [Call]()
+}
 
 extension CallStation: Station {
     func users() -> [User] {
-        []
+        userArray
     }
     
     func add(user: User) {
-
+        if(!userArray.contains(user)) {
+            userArray.append(user)
+        }
     }
     
     func remove(user: User) {
-
+        if(userArray.contains(user)) {
+            userArray.remove(at: userArray.firstIndex(of: user)!)
+        }
     }
     
     func execute(action: CallAction) -> CallID? {
